281c48a581b7a25cc3a86054fb1f53a4ce132126
nullpterexcept <sihanw@nycstudents.net>
2025-05-29 14:58:07 -0400

    Create cos.py

---
+++ cos.py
@@ -0,0 +1,11 @@
+epsilon = 1e-15
+def cos(x):
+  res = 0
+  term = 1
+  for n in range(1, 100):
+    res += term
+    term *= (-1)*(x**2)
+    term /= (2*n-1)*(2*n)
+    if abs(term) < epsilon:
+      break
+  return res
