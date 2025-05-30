d75d6b90c5d8ea72098efabb59559d10b2ba2876
lucasl53-stuy <lucasl53@nycstudents.net>
2025-05-30 17:47:31 +0000

    factorial and e

---
+++ constants.py
@@ -0,0 +1,16 @@
+def sqrt(n):
+  a = n ** .5
+  return a
+
+def factorial(n):
+  if n == 0:
+    return 1
+  a = 1
+  for i in range (1,n + 1):
+    a = a * i
+  return a
+
+e = 0
+for i in range(15):
+  e += 1/factorial(i)
+print(e) 
