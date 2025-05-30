d5a6a03439f2b63bed0bab16c0bb66c9438bfecf
nullpterexcept <sihanw@nycstudents.net>
2025-05-30 18:07:20 +0000

    inverse trig functions

--- arctan.py
+++ arctan.py
@@ -1,2 +1,15 @@
+from constants import pi
+epsilon = 1e-15
 def arctan(x):
-    
+  if x > 1:
+    return -arctan(1/x)+pi/2
+  if x < -1:
+    return -arctan(1/x)-pi/2
+  res = 0
+  term = x
+  for n in range(1, 100):
+    res += term/(2*n-1)
+    term *= (-1)*(x**2)
+    if abs(term/(2*n+1)) < epsilon:
+      break
+  return res
